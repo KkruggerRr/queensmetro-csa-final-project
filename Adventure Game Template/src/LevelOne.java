f7c365bd7f35abedd599022d21820214c0b809c3
KkruggerRr <ChristianC720@outlook.com>
2025-06-04 18:05:40 +0000

    code

---
+++ Adventure Game Template/src/LevelOne.java
@@ -0,0 +1,10 @@
+class LevelOne extends AdventureGame {
+    AdventureGame game;
+    LevelOne(AdventureGame game) {
+        this.game = game;
+    }
+    public void play() {
+        System.out.println("Level 1: ???");
+       
+        }
+    }
